80bfbc458c1d4d992d74ce9f22d619893655a1c5
Dhruvil Lakhtaria <lakhtariadhruvil123@gmail.com>
2023-02-20 11:09:47 +0530

    publish website

---
+++ client/src/components/Homepage/Home.js
@@ -0,0 +1,20 @@
+import TopSection from "./TopSection";
+import MainSection from "./MainSection";
+import Footer from "./Footer";
+import Copyrights from "./Copyrights";
+
+
+
+
+const Home = () => {
+  return (
+    <div className="homepage">
+      <TopSection />
+      <MainSection />
+      <Footer />
+      <Copyrights />
+    </div>
+  );
+};
+
+export default Home;
